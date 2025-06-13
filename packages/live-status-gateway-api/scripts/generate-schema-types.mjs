6f2658059dca625ef445e03918fbd8a1f5472cce
Johan Nyman <johan@nytamin.se>
2025-06-13 09:54:01 +0200

    chore: generate-schema-types: add prettier step to format generated types (#1457)

--- packages/live-status-gateway-api/scripts/generate-schema-types.mjs
+++ packages/live-status-gateway-api/scripts/generate-schema-types.mjs
@@ -1,6 +1,8 @@
 import { TypeScriptGenerator } from '@asyncapi/modelina'
 import { fromFile, Parser } from '@asyncapi/parser'
 import fs from 'fs/promises'
+import cp from 'child_process'
+import * as path from 'path'
 
 const BANNER =
 	'/* eslint-disable */\n/**\n * This file was automatically generated using and @asyncapi/parser @asyncapi/modelina.\n * DO NOT MODIFY IT BY HAND. Instead, modify the source AsyncAPI schema files,\n * and run "yarn generate-schema-types" to regenerate this file.\n */\n'
@@ -82,6 +84,29 @@ for (const model of models) {
 const allModelsString =
 	BANNER + '\n\n' + allmodelContent.join('\n\n') + '\n\n' + 'export {' + allModelNames.join(', ') + '};'
 
-await fs.writeFile('src/generated/schema.ts', allModelsString)
+const fileName = path.resolve('src/generated/schema.ts')
+await fs.writeFile(fileName, allModelsString)
 
-console.log('Schema types written to src/generated/schema.ts')
+// Prettier format the output file:
+await runCmd(`npx prettier --write "${fileName}"`, {
+	// Run from repo root, so that prettier picks up the config
+	cwd: path.resolve('../..'),
+})
+
+console.log(`Schema types written to ${fileName}`)
+
+async function runCmd(cmd, options) {
+	await new Promise((resolve, reject) => {
+		const child = cp.exec(cmd, options || {}, (err, stdout, stderr) => {
+			if (err) {
+				console.error('stderr', stderr)
+				reject(err)
+			} else {
+				resolve(stdout)
+			}
+		})
+
+		child.stdout.pipe(process.stdout)
+		child.stderr.pipe(process.stderr)
+	})
+}

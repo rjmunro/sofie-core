6060e7e2645dfbc19fde263de35f545d9200e02c
Johan Nyman <johan@nytamin.se>
2025-09-04 10:25:10 +0200

    fix(PGW): handle situation when device is not initialized yet
    At that point in time, the device._device is not set.

--- packages/playout-gateway/src/tsrHandler.ts
+++ packages/playout-gateway/src/tsrHandler.ts
@@ -280,6 +280,7 @@ export class TSRHandler {
 		this.tsr.connectionManager.on('connectionEvent:connectionChanged', (id, status) => {
 			const coreTsrHandler = this._coreTsrHandlers[id]
 			if (!coreTsrHandler) return
+			if (!coreTsrHandler._device) return // Not initialized yet
 
 			coreTsrHandler.statusChanged(status)
 
@@ -382,7 +383,7 @@ export class TSRHandler {
 		this.tsr.connectionManager.on('connectionEvent:debugState', (id, state) => {
 			const device = this._coreTsrHandlers[id]?._device
 
-			if (device?.debugState && this._coreHandler.logDebug) {
+			if (device && device.debugState && this._coreHandler.logDebug) {
 				// Fetch the Id that core knows this device by
 				const coreId = this._coreTsrHandlers[device.deviceId].core.deviceId
 				this._debugStates.set(unprotectString(coreId), state)

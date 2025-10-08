7154a3f52d63ed80533fc0def6e894eb9b1513f7
Robert (Jamie) Munro <rjmunro@arjam.net>
2025-10-08 13:57:19 +0100

    fix: Allow determining TSR version in node 20
    Without breaking node 16
    
    This prevents the error:
    
    TypeError [ERR_IMPORT_ATTRIBUTE_MISSING]: Module "file:///Users/rjmunro/superflytv/sofie-timeline-state-resolver/packages/timeline-state-resolver-types/package.json" needs an import attribute of "type: json"
        at validateAttributes (node:internal/modules/esm/assert:88:15)
        at defaultLoadSync (node:internal/modules/esm/load:179:3)
        at ModuleLoader.getModuleJobForRequire (node:internal/modules/esm/loader:379:24)
        at new ModuleJobSync (node:internal/modules/esm/module_job:341:34)
        at ModuleLoader.importSyncForRequire (node:internal/modules/esm/loader:326:11)
        at loadESMFromCJS (node:internal/modules/cjs/loader:1411:24)
        at Module._compile (node:internal/modules/cjs/loader:1544:5)
        at Object..js (node:internal/modules/cjs/loader:1699:10)
        at Module.load (node:internal/modules/cjs/loader:1313:32)
        at Function._load (node:internal/modules/cjs/loader:1123:12) {
      code: 'ERR_IMPORT_ATTRIBUTE_MISSING'
    
    Which I got when trying to build blueprints using node 20 when I was 
    yarn link`ed to sofie-core, rather than using a pre-built version.

--- packages/shared-lib/src/tsr.ts
+++ packages/shared-lib/src/tsr.ts
@@ -1,5 +1,10 @@
 import * as TSR from 'timeline-state-resolver-types'
 export { TSR }
 
-import * as tsrPkgInfo from 'timeline-state-resolver-types/package.json'
-export const TSR_VERSION: string = tsrPkgInfo.version
+// Dynamically import package.json to avoid import attributes error
+const TSR_VERSION = require('timeline-state-resolver-types/package.json').version
+
+// Below line should work in Node.js 18+ and with bundlers that support import attributes, and replace the above
+// import { version as TSR_VERSION } from 'timeline-state-resolver-types/package.json' with { type: 'json' }
+
+export { TSR_VERSION }
